2a09b9e024ddcbef5cf4b8444cfb9e8b6d79fb26
eduardc77 <>
2025-07-23 00:07:05 +0300

    chore: add initial Xcode project setup

---
+++ GitLabExplorer/GitLabExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLabExplorerApp.swift
+//  GitLabExplorer
+//
+//  Created by User on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct GitLabExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitLabExplorerTests/GitLabExplorerTests.swift
@@ -0,0 +1,16 @@
+//
+//  GitLabExplorerTests.swift
+//  GitLabExplorerTests
+//
+//  Created by User on 7/23/25.
+//
+
+import Testing
+
+struct GitLabExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
